deb433d8259a098eab7af9b52d041394114a0748
Awesome <awesome_devnet@outlook.com>
2023-06-23 20:20:16 +0900

    .

--- Office/Office/MainWindow.xaml.cs
+++ Office/Office/MainWindow.xaml.cs
@@ -22,25 +22,43 @@ namespace Office {
     /// </summary>
     /// 
 
-    public partial class MainWindow : Window {        
+    public partial class MainWindow : Window {
+
+        
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
             List<Room> Rooms = new List<Room>();
-            Rooms.Add(new Room { Name = "사무실1", Width = 10, Height = 10, myRectangle = Office1});
-            Rooms.Add(new Room { Name = "사무실2", Width = 20, Height = 20, myRectangle = Office2 });
-            Rooms.Add(new Room { Name = "사무실3", Width = 30, Height = 30, myRectangle = Office3 });
+
+            Rooms.Add(new Room("사무실1", Office1));
+            Rooms.Add(new Room("사무실2", Office2)));
+            Rooms.Add(new Room("사무실3", Office3)));
 
             OfficeList.ItemsSource = Rooms;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e) {
+            
         }
     }
 
     public class Room {
         public string Name { get; set; }
-        public double Width { get; set; }
-        public double Height { get; set; }
+        public double Width {
+            get; set; 
+        }
+        public double Height {
+            get; set;
+        }
         public double Area { get; set; }
         public int Tiles { get; set; }
 
         public Rectangle myRectangle;
+
+        public Room(string name, Rectangle rect) {
+            rect.SizeChanged += (s, e) => {
+                Height = myRectangle.ActualHeight;
+                Width = myRectangle.ActualWidth;
+            };
+        }
     }
 }
