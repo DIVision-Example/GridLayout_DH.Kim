938122d58292da27f2c1f29372a7a62729371ee7
Awesome <awesome_devnet@outlook.com>
2023-06-23 16:35:12 +0900

    _feat

---
+++ WpfApp1/Changer.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1 {
+    /// <summary>
+    /// Interaction logic for Changer.xaml
+    /// </summary>
+    public partial class Changer : Window {
+        public Changer() {
+            InitializeComponent();
+        }
+
+        public Changer(FrameworkElement target) {
+            lblb.Text = ((TextBlock)target).Text;
+        }
+
+        private void newwindow_Initialized(object sender, EventArgs e) {
+            
+        }
+    }
+}
