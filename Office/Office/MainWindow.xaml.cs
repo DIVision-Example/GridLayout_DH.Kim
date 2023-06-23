48494061b54814b546a982ae96b71fef9aeabbce
Awesome <awesome_devnet@outlook.com>
2023-06-23 19:58:24 +0900

    _

--- Office/Office/MainWindow.xaml.cs
+++ Office/Office/MainWindow.xaml.cs
@@ -39,6 +39,7 @@ namespace Office {
         public double Width { get; set; }
         public double Height { get; set; }
         public double Area { get; set; }
+
         public Rectangle myRectangle;
     }
 }
